a8f84cea481cf84d08aceeef814486d814d01e11
Mark Northup <marknorthup28@gmail.com>
2023-09-14 14:22:30 -0500

    update

---
+++ Desktop/Code/firstWebsite/javascript.js
@@ -0,0 +1,11 @@
+var body = document.querySelector('body');
+var button = document.querySelector('button');
+
+body.addEventListener('click', function() {
+  alert("YOU CLICKED ON THE BODY!!!!");
+});
+
+
+button.addEventListener('click', function() {
+  alert("YOU'RE REALLY PUSHING MY BUTTONS!!!!!");
+});
